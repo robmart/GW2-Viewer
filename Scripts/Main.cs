d49ea272a16390f3152f0433c61596f2c742f7b6
Robin <robinmartensson01@gmail.com>
2020-08-06 18:25:48 +0200

    Now gets correct connection

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -1,17 +1,29 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using Godot;
 
 namespace GW2Viewer.Scripts {
 	public class Main : Control {
-		public static APIConnection Connection;
+		public static APIConnection Connection {
+			get {
+				var connection = Connections[0];
+
+				foreach (var connection2 in Connections.Where(connection2 => connection2.IsSelected)) {
+					connection = connection2;
+					break;
+				}
+
+				return connection;
+			}
+		}
 		public static Color         ButtonColor { get; set; } = new Color(0.66f, 0.66f, 0.66f);
 
 		public static List<APIConnection> Connections { get; } = new List<APIConnection>();
 
 		public override void _Ready() {
 			base._Ready();
-			Connection = new APIConnection {IsSelected = true, CanBeDeleted = false};
+			new APIConnection {IsSelected = true, CanBeDeleted = false};
 
 			Update();
 		}
